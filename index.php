dc7d4d5dde76c1cf7665a93887ee5b3d67d3912e
unknown <brosjayme@gmail.com>
2023-05-25 07:57:19 +0100

    bootstrap modal added

--- index.php
+++ index.php
@@ -65,14 +65,13 @@
 </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
 
 <script>
-
 function adduser(){
  var nameAdd=$('#complename').val();
  var emailAdd=$('#complemail').val();
