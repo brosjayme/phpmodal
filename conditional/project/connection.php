c4879a79821ea987dbbd654d189c557ca52a9cb2
unknown <brosjayme@gmail.com>
2023-10-04 09:17:57 +0100

    added index pagep

--- conditional/project/connection.php
+++ conditional/project/connection.php
@@ -1,7 +1,8 @@
+<?php
 $server    = "localhost";
 $username  = "root";
-$password  = "root";
-$db        = "my_first_database";
+$password  = "";
+$db        = "first_db";
 
 
 //create a connection 
@@ -12,3 +13,5 @@ if(!$conn) {
     die("connection failed:" .  mysqli_connect_error() );
 }
 echo "connection successfull!";
+
+?>
